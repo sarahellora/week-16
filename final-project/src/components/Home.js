49ccfa2fcdfac606f7d27ef89a15c32cde2cbf33
Sarahellora <sarahellora1996@gmail.com>
2023-02-23 14:55:58 -0700

    added update form

--- final-project/src/components/Home.js
+++ final-project/src/components/Home.js
@@ -1,56 +1,73 @@
-import React, {useEffect,useState}from 'react'
-import axios from 'axios';
-import BookList from './BookList';
-import {useHistory} from 'react-router-dom'
-
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+import BookList from "./BookList";
 
 const Home = () => {
-    const [initialData, setInitialData] = useState()
-    const history = useHistory();
+  const [initialData, setInitialData] = useState();
+  const [showForm, setShowForm] = useState(false);
 
+  useEffect(() => {
+    axios
+      .get("https://63f4610d2213ed989c416cd7.mockapi.io/users")
+      .then((response) => {
+        setInitialData(response.data);
+      });
+  }, []);
 
-    useEffect(() => {
-      axios.get('https://63f4610d2213ed989c416cd7.mockapi.io/users')
-          .then(response =>{
-                  setInitialData(response.data)
-              }
-          );
-      },[])
-      function handleDelete(id){
-        axios.delete(`https://63f4610d2213ed989c416cd7.mockapi.io/users/${id}`).then(() => setInitialData(
+  function handleDelete(id) {
+    axios
+      .delete(`https://63f4610d2213ed989c416cd7.mockapi.io/users/${id}`)
+      .then(() =>
+        setInitialData(
           initialData.filter((post) => {
-             return post.id !== id;
-          })) )
-        }
-  
-      function handleAdd(e,author,title){
-        e.preventDefault();
-        axios.post('https://63f4610d2213ed989c416cd7.mockapi.io/users',{title:title,author:author}).then((response) => {
-          setInitialData([response.data, ...initialData]);
-       });
-  
-      } 
-      const handleUpdate = (e,author,title,id) => {
-         axios.put(`https://63f4610d2213ed989c416cd7.mockapi.io/users/${id}`,{title:title,author:author}).then((response) => {
-          setInitialData([response.data, ...initialData]);
-       });
-      }
+            return post.id !== id;
+          })
+        )
+      );
+  }
+
+  function handleAdd(e, author, title) {
+    e.preventDefault();
+    axios
+      .post("https://63f4610d2213ed989c416cd7.mockapi.io/users", {
+        title: title,
+        author: author,
+      })
+      .then((response) => {
+        setInitialData([response.data, ...initialData]);
+      });
+  }
+  const handleUpdate = (e, author, title, id) => {
+    e.preventDefault();
+    axios
+      .put(`https://63f4610d2213ed989c416cd7.mockapi.io/users/${id}`, {
+        title: title,
+        author: author,
+      })
+      .then((response) => {
+        const filterArray = initialData.filter((post) => {
+          return post.id !== id;
+        });
+        setInitialData([response.data, ...filterArray]);
+        setShowForm(false);
+      });
+  };
   return (
     <div className="home">
-         {initialData?.map((book, index)=>
-            <BookList
-            key={index}
-            title={book.title}
-            author={book.author}
-            handleDelete={handleDelete}
-            handleUpdate={handleUpdate}
-            id={book.id}
-            />
-      )}
-
-   
+      {initialData?.map((book, index) => (
+        <BookList
+          key={index}
+          title={book.title}
+          author={book.author}
+          handleDelete={handleDelete}
+          handleUpdate={handleUpdate}
+          id={book.id}
+          showForm={showForm}
+          setShowForm={setShowForm}
+        />
+      ))}
     </div>
   );
-}
- 
+};
+
 export default Home;
--- final-project/src/components/UpdateForm.js
+++ final-project/src/components/UpdateForm.js
@@ -1,46 +1,48 @@
-import React, {useEffect,useState}from 'react'
-import Button from 'react-bootstrap/Button';
-import Form from 'react-bootstrap/Form';
+import React, { useEffect, useState } from "react";
+import Button from "react-bootstrap/Button";
+import Form from "react-bootstrap/Form";
 
+const UpdateForm = (props) => {
+  const { id, handleUpdate, title, author } = props;
 
+  const [formTitle, setFormTitle] = useState(title);
+  const [formAuthor, setFormAuthor] = useState(author);
 
-const UpdateForm = (props) => {   
-    const {id,handleUpdate, title,author} = props
-   
-    const [formTitle,setFormTitle]=useState(title)
-    const [formAuthor,setFormAuthor]=useState(author)
-    
-      const handleAuthor = (e) =>{
-        const author = e.target.value
-        setFormAuthor(author)
-      }
-      const handleTitle = (e) =>{
-        const title = e.target.value
-        setFormTitle(title)
-      }
+  const handleAuthor = (e) => {
+    const author = e.target.value;
+    setFormAuthor(author);
+  };
+  const handleTitle = (e) => {
+    const title = e.target.value;
+    setFormTitle(title);
+  };
 
-      // const handleSubmit = (e,formAuthor,formTitle,id)=>{
-      //   console.log(formAuthor,formTitle,id)
-      //   handleUpdate(formAuthor,formTitle,id)
-      // }
-
-
-return (
-<>
-<Form onSubmit={(e)=>handleUpdate(e,formAuthor,formTitle,id)}>
-      <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
-      <Form.Label>Book Author</Form.Label>
-        <Form.Control as="textarea" value={formTitle}  placeholder='Title' onChange={(e)=> handleTitle(e)} />
-      </Form.Group>
-      <Form.Group className="mb-3" controlId="exampleForm.ControlTextarea1">
-        <Form.Label>BooK Title</Form.Label>
-        <Form.Control as="textarea" value={formAuthor}  placeholder='Author' onChange={(e)=> handleAuthor(e)} />
-      </Form.Group>
-      <Button type="submit" variant="success">Submit
-      </Button>
-    </Form>
-</>
-
-    )
-}
-export default UpdateForm
+  return (
+    <>
+      <Form onSubmit={(e) => handleUpdate(e, formAuthor, formTitle, id)}>
+        <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
+          <Form.Label>Book Author</Form.Label>
+          <Form.Control
+            as="textarea"
+            value={formTitle}
+            placeholder="Title"
+            onChange={(e) => handleTitle(e)}
+          />
+        </Form.Group>
+        <Form.Group className="mb-3" controlId="exampleForm.ControlTextarea1">
+          <Form.Label>BooK Title</Form.Label>
+          <Form.Control
+            as="textarea"
+            value={formAuthor}
+            placeholder="Author"
+            onChange={(e) => handleAuthor(e)}
+          />
+        </Form.Group>
+        <Button type="submit" variant="success">
+          Submit
+        </Button>
+      </Form>
+    </>
+  );
+};
+export default UpdateForm;
